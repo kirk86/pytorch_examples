b3351af339da692a68b7ac582d627fd9d963f3e9
jm <kirk86@walla.com>
2017-03-31 04:25:24 +0100

    init commit

--- 4.Feedforward_Neural_Network/ffnn_gpu.py
+++ 4.Feedforward_Neural_Network/ffnn_gpu.py
@@ -3,67 +3,73 @@ import torch.nn as nn
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-# Hyper parameters
+# Hyper-parameters
+
 input_size = 784
+hidden_size = 500
 num_classes = 10
 num_epochs = 5
 batch_size = 100
-learning_rate = 0.001
+learning_rate = 1e-3
 
-# MNIST dataset (images and labels)
+# MNIST dataset
 train_dataset = datasets.MNIST(root='./data',
                                train=True,
                                transform=transforms.ToTensor(),
                                download=True)
 
 test_dataset = datasets.MNIST(root='./data',
-                              train=False,
-                              transform=transforms.ToTensor())
+                              train=True,
+                              transform=transforms.ToTensor(),
+                              download=True)
 
-# Dataset loader (Input Pipeline)
+# Data loader (Input pipeline)
 train_loader = tch.utils.data.DataLoader(dataset=train_dataset,
                                          batch_size=batch_size,
                                          shuffle=True)
 
 test_loader = tch.utils.data.DataLoader(dataset=test_dataset,
                                         batch_size=batch_size,
-                                        shuffle=True)
+                                        shuffle=False)
 
-# Model
-class LogisticRegression(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(LogisticRegression, self).__init__()
-        self.linear = nn.Linear(input_size, num_classes)
 
+# Feedforward Neural Network (1 hidden layer)
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
-        out = self.linear(x)
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
         return out
 
 
-model = LogisticRegression(input_size, num_classes)
+net = Net(input_size, hidden_size, num_classes)
+net.cuda()
 
-# Set loss + optimizer
-# Softmax is internally computed
-# set parameters to be updated
+# Loss and Optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = tch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = tch.optim.Adam(net.parameters(), lr=learning_rate)
 
-# training model
 for epoch in xrange(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-        images = Variable(images.view(-1, 28 * 28))
-        labels = Variable(labels)
+        # Convert torch tensor to variable
+        images = Variable(images.view(-1, 28 * 28)).cuda()
+        labels = Variable(labels).cuda()
 
         # forward + backward + optimize
         optimizer.zero_grad()
-        outputs = model(images)
+        outputs = net(images)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
         if (i + 1) % 100 == 0:
-            print("Epoch: [{}/{}], Step: [{}/{}], Loss: {:4f}"
+            print("Epoch [{}/{}], Step [{}/{}], Loss: {:4f}"
                   .format(epoch + 1, num_epochs, i + 1,
                           len(train_dataset)//batch_size, loss.data[0]))
 
@@ -71,14 +77,14 @@ for epoch in xrange(num_epochs):
 correct = 0
 total = 0
 for images, labels in test_loader:
-    images = Variable(images.view(-1, 28*28))
-    outputs = model(images)
+    images = Variable(images.view(-1, 28 * 28)).cuda()
+    outputs = net(images).cuda()
     _, predicted = tch.max(outputs.data, 1)
     total += labels.size(0)
     correct += (predicted == labels).sum()
 
-print("Accuracy of the model on the 10000 test images: {} %%"
+print("Accuracy of the network on the 10000 test images: {}%"
       .format(100 * correct / total))
 
 # save the model
-# tch.save(model.state_dict(), 'model.pkl')
+# tch.save(net.state_dict(), 'model.pkl')
